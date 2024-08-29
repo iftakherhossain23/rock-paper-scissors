af836d12e4ae1bb100c8dec4d2b46f34df00f264
ifty-h <83318660+ifty-h@users.noreply.github.com>
2024-08-29 14:15:35 -0400

    Add playRound function

--- script.js
+++ script.js
@@ -15,3 +15,42 @@ function getHumanChoice(){
         alert("Invalid choice.");
     }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,computerChoice){
+    if(humanChoice == "rock"){
+        if(computerChoice == "rock") console.log("Tie! Rock and Rock.");
+        else if(computerChoice == "paper"){
+            console.log("You lose! Paper beats Rock.");
+            ++computerScore;
+        }
+        else if(computerChoice == "scissors"){
+            console.log("You win! Rock beats Scissors.");
+            ++humanScore;
+        }
+    }
+    else if(humanChoice == "paper"){
+        if(computerChoice == "rock"){
+            console.log("You lose! Paper beats Rock.");
+            ++humanScore;
+        }
+        else if(computerChoice == "paper") console.log("Tie! Paper and Paper.");
+        else if(computerChoice == "scissors"){
+            console.log("You lose! Scissors beats Paper.");
+            ++computerScore;
+        }
+    }
+    else if(humanChoice == "scissors"){
+        if(computerChoice == "rock"){
+            console.log("You lose! Rock beats Scissors.");
+            ++computerScore;
+        } 
+        else if(computerChoice == "paper"){
+            console.log("You win! Scissors beats Paper.");
+            ++humanScore;
+        }
+        else if(computerChoice == "scissors") console.log("Tie! Scissors beats Scissors.");
+    }
+}
